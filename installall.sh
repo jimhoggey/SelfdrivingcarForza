5a213c63a0b65ae8ae45377915d774a13eee2e96
jimhoggey <31178932+jimhoggey@users.noreply.github.com>
2021-11-05 11:20:36 +1100

    Create installall.sh

---
+++ installall.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+python --version
+sudo apt install software-properties-common -y
+sudo apt update -y
+sudo apt-get install python3.6
+python --version
+pip install --upgrade pip 
+pip install --upgrade setuptools 
+pip install matplotlib 
+pip install tensorflow 
+pip install opencv-python 
+pip install pickle 
+pip install moviepy 
+pip install glob 
+pip install numpy 
+pip install argparse
+pip install imutils 
+pip install Scripy 
+pip install ipython
+Pip install htop
+Pip install pickle
+Pip install argparse
+pip install moviepy
